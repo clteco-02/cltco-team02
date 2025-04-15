af015b2c28fef0e3db6c4a753c7b7be27b81cafe
binary-h0 <max000807@gmail.com>
2025-04-15 14:32:30 +0900

    Deploy: docker

--- backend-java/src/main/java/com/example/backend/config/WebConfig.java
+++ backend-java/src/main/java/com/example/backend/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOriginPatterns("https://team02-front.proudwave-56b3fa81.koreacentral.azurecontainerapps.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .allowedHeaders("*");
